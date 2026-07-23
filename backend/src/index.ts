036a04771fea4caa6af6a69ed207167ccebd0622
Aziz <d22it211@charusat.edu.in>
2026-07-23 20:39:27 +0530

    Bugs fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import nms from './mediaServer';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 import authRoutes from './routes/authRoutes';
 import { authMiddleware } from './middlewares/authMiddleware';
@@ -23,6 +24,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+// Proxy video stream requests to Node Media Server (Port 8000)
+app.use('/live', createProxyMiddleware({ target: 'http://127.0.0.1:8000', changeOrigin: true }));
 
 // Routes
 app.use('/api/auth', authRoutes);
